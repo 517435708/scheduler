917a70e09686bc269e765041e14ffab467a4d638
517435708 <lphusarz@gmail.com>
2021-02-11 02:04:12 +0100

    init commit

---
+++ src/test/java/com/santander/wm/autosavings/scheduler/SchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santander.wm.autosavings.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
